05e02402172e2991b639cb87ec3c1a91c5c4b08f
dmv302 <spaceshroom175@gmail.com>
2019-06-05 15:56:27 +0300

    Map-generation stuff.

---
+++ src/map_tools/Level.java
@@ -0,0 +1,8 @@
+package map_tools;
+
+/**
+ * All the available level numbers are stored here.
+ */
+public enum Level {
+	ONE, TWO, THREE, FOUR;
+}
---
+++ src/map_tools/Map.java
@@ -0,0 +1,95 @@
+package map_tools;
+
+import game_content.GameField;
+import game_objects.map_objects.MapObject;
+import game_objects.map_objects.walls.BrickWall;
+import game_objects.map_objects.walls.SteelWall;
+
+import java.util.ArrayList;
+
+/**
+ * Map Object that simply extends ArrayList with MapObjects
+ * Static method is used to acquire the needed level map
+ * <p>
+ * Integers are used for mapping. Here's the legend:
+ * 0 - empty
+ * 1 - brick wall
+ * 2 - steel wall
+ * 3 - water
+ * 4 - grass(?) cover
+ */
+public class Map extends ArrayList<MapObject> {
+
+	/**
+	 * Private constructor that generates map from the 2D array
+	 * @param map 2d Array. Use reference in a class description.
+	 */
+	private Map(int[][] map) {
+		for (int i = 0; i < GameField.MAP_SIZE; i++) {
+			for (int j = 0; j < GameField.MAP_SIZE; j++) {
+				switch (map[j][i]) {
+					case 1:
+						add(new BrickWall(i * GameField.BYTE, j * GameField.BYTE));
+						break;
+					case 2:
+						add(new SteelWall(i * GameField.BYTE, j * GameField.BYTE));
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Pre-made levels that match enums
+	 * @param level number of level we want to make
+	 * @return made Map for a level
+	 */
+	public static Map getLevelMap(Level level) {
+		int[][] map = null;
+		switch (level) {
+			case ONE:
+				map =
+						new int[][]
+								{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1},
+										{2, 2, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 2, 2},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+										{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+								};
+				break;
+			case TWO:
+				break;
+			case THREE:
+				break;
+			case FOUR:
+				break;
+		}
+		return new Map(map);
+	}
+}
