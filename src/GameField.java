565651db8cc988785b70593b31ffd444f0797a09
dmv302 <spaceshroom175@gmail.com>
2019-06-05 14:48:16 +0300

    Added working map drawing tool

--- src/GameField.java
+++ src/GameField.java
@@ -1,18 +1,32 @@
+import game_objects.map_objects.MapObject;
+import game_objects.map_objects.walls.BrickWall;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameField extends JPanel implements Runnable, ActionListener {
 
 	/**
+	 * Size of the game map relative to the tile size. Actually its twice as small relative to the tank because every map tile is divided into four destructible parts
+	 */
+	private final int MAP_SIZE = 26;
+	/**
+	 * HOLY CONSTANT - BYTE
+	 */
+	private final int BYTE = 8;
+	/**
 	 * Dimensions of a game field (height and width are always equal)
 	 */
-	private final int FIELD_DIMENSIONS = 256;
+	private final int FIELD_DIMENSIONS = MAP_SIZE * BYTE;
 	/**
 	 * Delay in miliseconds
 	 */
 	private final int DELAY = 25;
+	private List<MapObject> map = new ArrayList<>();
 
 	private Thread animator;
 
@@ -27,7 +41,13 @@ public class GameField extends JPanel implements Runnable, ActionListener {
 	private void initGameField() {
 		setBackground(Color.BLACK);
 		setPreferredSize(new Dimension(FIELD_DIMENSIONS, FIELD_DIMENSIONS));
+		setFocusable(true);
 
+		initMap();
+
+	}
+
+	private void initMap() {
 
 	}
 
@@ -52,6 +72,14 @@ public class GameField extends JPanel implements Runnable, ActionListener {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+
+		drawMapObjects(g);
+	}
+
+	private void drawMapObjects(Graphics g) {
+		for (MapObject mo : map)
+			if (mo.isVisible())
+				g.drawImage(mo.getImage(), mo.getX(), mo.getY(), this);
 
 	}
 
