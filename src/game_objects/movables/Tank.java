921f6960aa3c8e6b2443b63c2dc81d94eee5b6b0
dmv302 <spaceshroom175@gmail.com>
2019-06-09 11:50:19 +0300

    Collisions are working for now, but still a subject to change

--- src/game_objects/movables/Tank.java
+++ src/game_objects/movables/Tank.java
@@ -2,6 +2,7 @@ package game_objects.movables;
 
 import game_objects.Destructible;
 import game_objects.Sprite;
+import javafx.util.Pair;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -17,6 +18,7 @@ public class Tank extends Sprite implements Destructible {
 	 */
 	private BufferedImage[] directions;
 	private int dx, dy;
+	private Direction currentDir;
 
 	public Tank(int x, int y) {
 		super(x, y);
@@ -37,6 +39,7 @@ public class Tank extends Sprite implements Destructible {
 			directions[i] = scale(directions[i]);
 		}
 		image = directions[2];
+		currentDir = Direction.NORTH;
 		getImageDimensions();
 	}
 
@@ -48,23 +51,24 @@ public class Tank extends Sprite implements Destructible {
 	public void changeDirection(Direction dir) {
 		dx = 0;
 		dy = 0;
+		currentDir = dir;
 
 		switch (dir) {
 			case WEST:
 				image = directions[0];
-				dx = -2;
+				dx = -1;
 				break;
 			case EAST:
 				image = directions[1];
-				dx = 2;
+				dx = 1;
 				break;
 			case NORTH:
 				image = directions[2];
-				dy = -2;
+				dy = -1;
 				break;
 			case SOUTH:
 				image = directions[3];
-				dy = 2;
+				dy = 1;
 				break;
 		}
 	}
@@ -95,6 +99,17 @@ public class Tank extends Sprite implements Destructible {
 	public void move() {
 		setX(dx + getX());
 		setY(dy + getY());
+	}
+
+	/**
+	 * Method we use to see if tank can move. If it theoretically moved and collided with something, we don't move;
+	 * @return Returns bounds that tank <u><b>would have</b></u> if it moved at this moment.
+	 */
+	public Rectangle getTheoreticalBounds() {
+		Rectangle rect = getBounds();
+		rect.x+=dx;
+		rect.y+=dy;
+		return rect;
 	}
 
 }
