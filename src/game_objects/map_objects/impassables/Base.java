0460fc841e328dc2e1e5967d16a5e4eede006b79
dmv302 <spaceshroom175@gmail.com>
2019-06-10 23:55:13 +0300

    added defeated modifier for base

--- src/game_objects/map_objects/impassables/Base.java
+++ src/game_objects/map_objects/impassables/Base.java
@@ -4,6 +4,8 @@ import game_objects.Destructible;
 import game_objects.map_objects.MapObject;
 
 public class Base extends MapObject implements Destructible {
+	private boolean defeated = false;
+
 	public Base(int x, int y) {
 		super(x, y, true);
 
@@ -15,8 +17,15 @@ public class Base extends MapObject implements Destructible {
 		getImageDimensions();
 	}
 
+	public boolean isDefeated() {
+		return defeated;
+	}
+
 	@Override
 	public void destroy() {
 		loadImage("resources/sprites/map/base_defeated.png");
+		defeated = true;
 	}
+
+
 }
