4af043d2f34819d5c3fa801f3aea8f730547303e
dmv302 <spaceshroom175@gmail.com>
2019-06-12 20:50:52 +0300

    Working on enemy ai

--- src/game_objects/movables/EnemyTank.java
+++ src/game_objects/movables/EnemyTank.java
@@ -1,5 +1,7 @@
 package game_objects.movables;
 
+import java.util.Random;
+
 public class EnemyTank extends Tank {
 
 	public EnemyTank(int x, int y, Direction dir) {
@@ -9,12 +11,12 @@ public class EnemyTank extends Tank {
 
 	private void init() {
 		loadImage("resources/sprites/enemy_tank/tank_%s.png");
+		changeDirection(Direction.SOUTH);
 		getImageDimensions();
 	}
 
 	@Override
 	public void move() {
-//		changeDirection(Direction.values()[new Random().nextInt(Direction.values().length)]);
 		super.move();
 		fire();
 	}
