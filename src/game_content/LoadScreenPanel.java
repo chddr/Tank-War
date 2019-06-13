d9a2fe23c93c77166522ea3df3b9507333a95eee
EinErste <37027381+EinErste@users.noreply.github.com>
2019-06-13 03:27:01 +0300

    minor changes

--- src/game_content/LoadScreenPanel.java
+++ src/game_content/LoadScreenPanel.java
@@ -1,5 +1,7 @@
 package game_content;
 
+import resources_classes.ScaledImage;
+
 import javax.swing.*;
 
 import java.awt.*;
@@ -22,12 +24,17 @@ public class LoadScreenPanel extends JPanel {
     }
 
     private void addLoadText(int level){
-        JLabel text = new JLabel("Level " + level);
+        JLabel text = new JLabel("Stage " + level);
         text.setFont(new Font(fontName,0,80));
         text.setForeground(Color.WHITE);
         text.setBounds(125,75,800,500);
         text.setOpaque(false);
         add(text);
+
+//        JLabel gif = new JLabel(new ImageIcon(ScaledImage.create("resources/sprites/menu/loading_screen/loading.gif",150,150)));
+//        gif.setBounds(300,300,200,200);
+//        gif.setOpaque(false);
+//        add(gif);
     }
 
     private void addWinText(){
